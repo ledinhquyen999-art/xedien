1c7c37d7837dbcbb0d54098534719e05c64bf7a1
ledinhquyen999-art <ledinhquyen999@gmail.com>
2025-11-13 15:25:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 from flask import Flask
-import os
 import pandas as pd
+import os
 
 from google.oauth2.service_account import Credentials
-from googleapiclient.http import MediaFileUpload
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 
 app = Flask(__name__)
 
 CREDS_PATH = "creds.json"
-FOLDER_ID = os.getenv("DRIVE_FOLDER_ID")
+YOUR_EMAIL = "ledinhquyen999@gmail.com"   # Gmail của bạn
 
 
-def upload_excel_to_drive(file_path, file_name):
-    scopes = ["https://www.googleapis.com/auth/drive.file"]
-
-    creds = Credentials.from_service_account_file(
-        CREDS_PATH,
-        scopes=scopes
-    )
-
+def upload_excel(file_path, file_name):
+    scopes = ["https://www.googleapis.com/auth/drive"]
+    creds = Credentials.from_service_account_file(CREDS_PATH, scopes=scopes)
     service = build("drive", "v3", credentials=creds)
 
-    metadata = {
-        "name": file_name,
-        "parents": [FOLDER_ID]
-    }
-
+    # Upload vào Drive của Service Account
+    metadata = {"name": file_name}
     media = MediaFileUpload(
         file_path,
         mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
@@ -38,25 +30,38 @@ def upload_excel_to_drive(file_path, file_name):
         fields="id"
     ).execute()
 
-    return uploaded["id"]
+    file_id = uploaded["id"]
+
+    # Share lại file cho Gmail của bạn
+    service.permissions().create(
+        fileId=file_id,
+        body={
+            "type": "user",
+            "role": "writer",
+            "emailAddress": YOUR_EMAIL
+        }
+    ).execute()
+
+    return file_id
 
 
 @app.route("/")
 def home():
-    return "⚡ Flask Render + Google Drive API đang chạy OK!"
+    return "⚡ Flask đang chạy trên Render – Google Drive API OK!"
 
 
 @app.route("/upload-test")
 def upload_test():
     df = pd.DataFrame({
-        "Time": ["10:00", "10:05", "10:10"],
-        "Power (kW)": [12, 14, 15]
+        "Time": ["10:00", "10:10", "10:20"],
+        "Power": [12, 14, 15]
     })
 
     file_name = "test_upload.xlsx"
     df.to_excel(file_name, index=False)
 
-    file_id = upload_excel_to_drive(file_name, file_name)
+    file_id = upload_excel(file_name, file_name)
+
     return f"✔ Upload thành công!<br>File ID: {file_id}"
 
 
